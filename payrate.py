aab7122a02669de085c5c31c91299c537dc191ec
hilda <95862150+stoic-rose@users.noreply.github.com>
2023-09-28 11:30:59 -0400

    Add files via upload

---
+++ payrate.py
@@ -0,0 +1,13 @@
+hours = 0
+payRate = 0
+grossPay = 0
+
+print("Enter the number of hours worked.")
+hours = input()
+
+print("Enter the hourly pay rate.")
+payRate = input()
+
+grossPay == hours * payRate
+
+print("The gross pay is " + grossPay)
